36d6c0a3b1e4b5639dfe625f883829b74c958fc6
brtrduon <brianduongt@gmail.com>
2018-12-28 20:03:55 -0800

    refactor App component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className='ui container' style={{ marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component {
+    onSearchSubmit = term => {
+        console.log(term)
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                {/* we can technically call "onSubmit" in this case anything we want since we are not actually submitting anything in the App component */}
+            </div>
+        )
+    }
 }
 
 export default App
