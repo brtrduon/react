48aa8b78721f4cca13a7a985ebcf8102994dc83a
brtrduon <brianduongt@gmail.com>
2018-12-30 11:40:51 -0800

    map

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
     state = {
@@ -24,7 +25,7 @@ class App extends React.Component {
             <div className='ui container' style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
                 {/* we can technically call "onSubmit" in this case anything we want since we are not actually submitting anything in the App component */}
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images} />
             </div>
         )
     }
