92a5582cd3176b47f9316fada1bdf266c10e689f
brtrduon <brianduongt@gmail.com>
2019-01-06 16:55:08 -0800

    connect

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -4,26 +4,32 @@ const songsReducer = () => {
     return [
         {
             title: 'The Night We Met',
+            artist: 'Lord Huron',
             duration: '3:28'
         },
         {
             title: 'Ocean Eyes',
+            artist: 'Billie Eilish',
             duration: '3:20'
         },
         {
             title: 'Whatever It Takes',
+            artist: 'Imagine Dragons',
             duration: '3:21'
         },
         {
             title: 'Adore',
+            artist: 'Amy Shark',
             duration: '3:05'
         },
         {
             title: 'Hunger',
+            artist: 'Florence + The Machine',
             duration: '3:35'
         },
         {
             title: 'One Foot',
+            artist: 'Walk the Moon',
             duration: '4:21'
         },
     ]
